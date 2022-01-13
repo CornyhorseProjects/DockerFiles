51c62cd585b8dfe1b14deeabf825abecd4157742
Matt <matt@mattkingsbury.com>
2022-01-13 07:46:15 -0500

    Added Requirements and updated Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,5 @@
 FROM python:3
+copy requirements.txt ./
+RUN pip install --no-cache-dir -r requirements.txt
+COPY . .
+CMD ["python", "./hello-world.py"]
---
+++ hello-world.py
@@ -0,0 +1,11 @@
+import pandas
+import black
+import requests
+import psycopg2
+import sqlalchemy
+import openpyxl
+import numpy
+import matplotlib
+import datetime
+
+print(f"Successfully imported packages after build. UTC Now is: {datetime.datetime.utcnow()}")
